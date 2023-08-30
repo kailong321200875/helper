44d86560fcea3c4830fab47c286c7da1bab5ef78
kailong321200875 <321200875@qq.com>
2023-08-30 16:25:11 +0800

    init: init

--- .husky/lintstagedrc.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,cjs,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
-  'package.json': ['prettier --write']
-}
